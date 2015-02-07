4a77032259fe463d1d769e61fe7f089ad232b777
yh <15248004@qq.com>
2015-02-07 12:30:56 +0800

    first edit addCartItem()

--- src/model/cart.js
+++ src/model/cart.js
@@ -1,9 +1,13 @@
+var _ = require('lodash');
+var Item = require('./item');
 function Cart() {
     this.cartItems = [];
 }
 
-Cart.prototype.addCartItem = function(cooker) {
-
+Cart.prototype.addCartItem = function(countItem) {
+    var key = _.keys(countItem)[0];
+    var item = _.find(Item.all(), {barcode : key});
+    this.cartItems.push(item, countItem[key]);
 };
 
 module.exports = Cart;
