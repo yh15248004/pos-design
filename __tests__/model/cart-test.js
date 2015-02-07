61ed0d4918bda8245ca2c95ef6c8236b52f7783b
yh <15248004@qq.com>
2015-02-07 13:17:03 +0800

    refactor addCartItem() unit test

--- __tests__/model/cart-test.js
+++ __tests__/model/cart-test.js
@@ -1,5 +1,6 @@
 jest.dontMock('lodash');
 jest.dontMock('../../src/model/item');
+jest.dontMock('../../src/model/cart-item');
 jest.dontMock('../../src/model/cart');
 
 describe('Cart', function() {
@@ -11,7 +12,17 @@ describe('Cart', function() {
         var cart = new Cart();
         it('should return correct cartItems', function() {
             cart.addCartItem({ 'ITEM000000' : 20 });
-            expect(cart.cartItems).toEqual([{barcode : 'ITEM000000', name : '可口可乐350ml', util : '瓶', price : 3.00, brand : '可口可乐'}, 20]);
+            expect(cart.cartItems).toEqual([
+                {count: 20,
+                 item: {
+                    barcode: 'ITEM000000',
+                    brand: '可口可乐',
+                    name: '可口可乐350ml',
+                    price: 3,
+                    util: '瓶'
+                    }
+             }
+            ]);
         });
 
     });
