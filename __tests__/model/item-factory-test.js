009a612f0943652e7bc626059da7239fb12539bb
yh <15248004@qq.com>
2015-02-07 15:33:23 +0800

    edit createCartItems() & pass test

--- __tests__/model/item-factory-test.js
+++ __tests__/model/item-factory-test.js
@@ -1,12 +1,24 @@
+jest.dontMock('lodash');
+jest.dontMock('../../src/model/item-factory');
 jest.dontMock('../../src/model/item');
+jest.dontMock('../../src/model/cart-item');
 jest.dontMock('../../src/model/cart');
 
 describe('ItemFactory', function() {
 
-    describe('.createCartItem()', function() {
-        it('should return correct ', function() {
+    var ItemFactory = require('../../src/model/item-factory');
+    var Cart = require('../../src/model/cart');
 
+    describe('.createCartItems()', function() {
+        var cart = new Cart();
+        var countItems = [{ 'ITEM000000' : 20 },
+                          { 'ITEM000010' : 2 }];
+
+        it('should return correct count', function() {
+            var result = ItemFactory.createCartItems(cart, countItems);
+            expect(result[0].count).toEqual(20);
         });
+
     });
 
 });
