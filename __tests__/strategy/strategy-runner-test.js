e4c4a4eecd6c3fcc0fe453c1b01b1136fd036d57
yh <15248004@qq.com>
2015-02-09 00:50:30 +0800

    add StrategyRunner unit test

---
+++ __tests__/strategy/strategy-runner-test.js
@@ -0,0 +1,16 @@
+jest.autoMockOff();
+
+describe('StrategyRunner', function() {
+    var StrategyRunner = require('../../src/strategy/strategy-runner');
+    describe('.getStrategy()', function() {
+
+        it('should return correct strategy', function() {
+            var strategyType = 1;
+            var cartItems = '111';
+            var result = StrategyRunner.getStrategy(strategyType, cartItems);
+            expect(result.type).toBe(111);
+        });
+
+    });
+
+});
