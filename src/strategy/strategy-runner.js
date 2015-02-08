5706bfabd65afb4a45cb058a45109d478eafe885
yh <15248004@qq.com>
2015-02-08 23:35:35 +0800

    complete getStrategy() with if & else if

--- src/strategy/strategy-runner.js
+++ src/strategy/strategy-runner.js
@@ -1,8 +1,27 @@
+var StrategyOne = require('./strategy-one');
+var StrategyTwo = require('./strategy-two');
+var StrategyThree = require('./strategy-three');
+var StrategyFour = require('./strategy-four');
+var NoStrategy = require('./no-strategy');
+
+
 function StrategyRunner() {
 }
 
-StrategyRunner.getStrategy = function(type, cartItems) {
+StrategyRunner.getStrategy = function(strategyType, cartItems) {
+    var result = new NoStrategy(cartItems);
 
+    if(strategyType === 1) {
+        result = new StrategyOne(cartItems);
+    } else if(strategyType === 2) {
+        result = new StrategyTwo(cartItems);
+    } else if(strategyType === 3) {
+        result = new StrategyThree(cartItems);
+    } else if(strategyType === 4) {
+        result = new StrategyFour(cartItems);
+    }
+
+    return result;
 };
 
 module.exports = StrategyRunner;
