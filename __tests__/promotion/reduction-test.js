73ad9dd67ab85fa13cf89202ae9a0af57b54191c
yh <15248004@qq.com>
2015-02-09 21:02:16 +0800

    add Reduction unit test

---
+++ __tests__/promotion/reduction-test.js
@@ -0,0 +1,22 @@
+jest.dontMock('../../src/promotion/reduction');
+
+describe('Reduction', function() {
+
+    var Reduction = require('../../src/promotion/reduction');
+    var reduction = new Reduction(100, 5, 599);
+
+    describe('#buildPromotionName()', function() {
+        it('should return correct string', function() {
+            var result = reduction.buildPromotionName();
+            expect(result).toBe('');
+        });
+    });
+
+    describe('#getPromotionMoney()', function() {
+        it('should return correct number', function() {
+            var result = reduction.getPromotionMoney();
+            expect(result).toBe(25.00);
+        });
+    });
+
+});
