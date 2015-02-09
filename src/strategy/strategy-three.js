cdc428b3545b830fae93400da96d8d770a1d47be
yh <15248004@qq.com>
2015-02-09 15:19:27 +0800

    remove cartItems from StrategyThree()

--- src/strategy/strategy-three.js
+++ src/strategy/strategy-three.js
@@ -1,7 +1,6 @@
 var Strategy = require('./strategy');
 
-function StrategyThree(cartItems) {
-    Strategy.call(this, cartItems);
+function StrategyThree() {
 }
 StrategyThree.prototype = Object.create(Strategy.prototype);
 StrategyThree.prototype.constructor = StrategyThree;
