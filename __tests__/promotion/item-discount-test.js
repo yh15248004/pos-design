b416481fcc5cbecafcdd8b15baa1596d05e222f6
yh <15248004@qq.com>
2015-02-09 19:11:51 +0800

    add getPromotionMoney() unit test

--- __tests__/promotion/item-discount-test.js
+++ __tests__/promotion/item-discount-test.js
@@ -3,12 +3,19 @@ jest.dontMock('../../src/promotion/item-discount');
 describe('ItemDiscount', function() {
 
     var ItemDiscount = require('../../src/promotion/item-discount');
-    var discount = new ItemDiscount(0.95, 50, '苹果');
+    var discount = new ItemDiscount(0.95, 50.9, '苹果');
 
     describe('#buildPromotionName()', function() {
         it('should return correct string', function() {
             var result = discount.buildPromotionName();
             expect(result).toBe('苹果单品打折');
+        });
+    });
+
+    describe('#getPromotionMoney()', function() {
+        it('should return correct number', function() {
+            var result = discount.getPromotionMoney();
+            expect(result).toBe(2.55);
         });
     });
 
