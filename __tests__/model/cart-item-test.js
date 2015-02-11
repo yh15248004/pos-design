2bf7666e9b38cc382cfc0e3fa5c079b219255a12
yh <15248004@qq.com>
2015-02-11 16:34:58 +0800

    add getBrand() unit test

---
+++ __tests__/model/cart-item-test.js
@@ -0,0 +1,20 @@
+jest.dontMock('../../src/model/item');
+jest.dontMock('../../src/model/cart-item');
+
+describe('CartItem', function() {
+
+    var Item = require('../../src/model/item');
+    var CartItem = require('../../src/model/cart-item');
+
+    beforeEach(function() {
+        cartItem = new CartItem(Item.all()[0], 20);
+    });
+
+    describe('#getBrand()', function() {
+        it('should return correct brand', function() {
+            var result = cartItem.getBrand();
+            expect(result).toBe('可口可乐');
+        });
+    });
+
+});
