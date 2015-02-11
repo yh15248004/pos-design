ffea4997aaa64befcafba55b8c5476a3c5e24d08
yh <15248004@qq.com>
2015-02-11 17:40:48 +0800

    add findDiscountItems() unit test

--- __tests__/strategy/strategy-one-test.js
+++ __tests__/strategy/strategy-one-test.js
@@ -3,6 +3,8 @@ jest.autoMockOff();
 describe('StrategyOne', function() {
 
     var StrategyOne = require('../../src/strategy/strategy-one');
+    var Item = require('../../src/model/item');
+    var CartItem = require('../../src/model/cart-item');
     var DiscountHouse = require('../../src/promotion/discount-house');
 
     describe('items()', function() {
@@ -14,6 +16,17 @@ describe('StrategyOne', function() {
 
         it('should return correct num', function() {
             expect(result[0].rate).toBe(0.95);
+        });
+
+    });
+
+    describe('.findDiscountItems()', function() {
+        var cartItems = [new CartItem(new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐'), 20),
+            new CartItem(new Item('ITEM000010', '可口可乐550ml', '瓶', 4.00, '可口可乐'), 12)];
+
+        it('should return correct discountItems', function() {
+            var result = StrategyOne.findDiscountItems(cartItems);
+            expect(result).toBe([{name: '可口可乐350ml', rate: 0.95}]);
         });
 
     });
