777f66f80edffda0ae87f4332490609936d97749
yh <15248004@qq.com>
2015-02-11 15:23:20 +0800

    add getItemDiscountInfo() & items()

--- src/strategy/strategy-one.js
+++ src/strategy/strategy-one.js
@@ -1,3 +1,4 @@
+var _ = require('lodash');
 var Strategy = require('./strategy');
 
 function StrategyOne() {
@@ -7,8 +8,30 @@ StrategyOne.prototype.constructor = StrategyOne;
 
 StrategyOne.prototype.getPromotionInfo = function(cartItems) {
     var promotionInfo = '';
-    
+    var discountCartItem = StrategyOne.getDiscountItem(cartItems);
+    var discountBrandCartItems = StrategyOne.getBrandCartItems(cartItems);
+    if(!StrategyOne.isSyndrome(discountCartItem, discountBrandCartItems)) {
+
+    }
+
     return promotionInfo;
+};
+
+StrategyOne.items =function() {
+    return [new DiscountHouse('可口可乐350ml', 0.95)];
+};
+
+StrategyOne.brands =function() {
+    return [new DiscountHouse('可口可乐', 0.9)];
+};
+
+StrategyOne.getItemDiscountInfo = function(cartItems) {
+    var discountInfo = '';
+    var discountCartItem = StrategyOne.getDiscountItem(cartItems);
+    if(!StrategyOne.isSyndrome(discountCartItem, StrategyOne.brands())) {
+
+    }
+    return discountInfo;
 };
 
 module.exports = StrategyOne;
