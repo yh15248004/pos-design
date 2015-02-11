a63e6478db898e3b176988e60bf2856f9ed7050d
yh <15248004@qq.com>
2015-02-11 11:44:54 +0800

    add WholeReduction() unit test

---
+++ __tests__/promotion/whole-reduction-test.js
@@ -0,0 +1,23 @@
+jest.dontMock('../../src/promotion/reduction');
+jest.dontMock('../../src/promotion/whole-reduction');
+
+describe('WholeReduction', function() {
+
+    var WholeReduction = require('../../src/promotion/whole-reduction');
+    var reduction = new WholeReduction(100, 5, 599);
+
+    describe('#buildPromotionName()', function() {
+        it('should return correct string', function() {
+            var result = reduction.buildPromotionName();
+            expect(result).toBe('满100减5');
+        });
+    });
+
+    describe('#getPromotionMoney()', function() {
+        it('should return correct number', function() {
+            var result = reduction.getPromotionMoney();
+            expect(result).toEqual('25.00');
+        });
+    });
+
+});
