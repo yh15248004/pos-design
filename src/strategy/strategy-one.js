decc62dbf31f0bff703222a1d3d5db5afe96e35b
yh <15248004@qq.com>
2015-02-11 13:31:47 +0800

    build getPromotionInfo() to wait compire

--- src/strategy/strategy-one.js
+++ src/strategy/strategy-one.js
@@ -5,4 +5,10 @@ function StrategyOne() {
 StrategyOne.prototype = Object.create(Strategy.prototype);
 StrategyOne.prototype.constructor = StrategyOne;
 
+StrategyOne.prototype.getPromotionInfo = function(cartItems) {
+    var promotionInfo = '';
+    
+    return promotionInfo;
+};
+
 module.exports = StrategyOne;
