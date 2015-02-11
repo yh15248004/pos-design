ce6e7654eb1b7c100b2442eb78afb41a1b9a9ce0
yh <15248004@qq.com>
2015-02-11 15:32:46 +0800

    add ReductionHouse into the project

---
+++ src/promotion/reduction-house.js
@@ -0,0 +1,6 @@
+function ReductionHouse(name, reachPoint, reduceMoney) {
+    this.name = name;
+    this.reachPoint = reachPoint;
+    this.reduceMoney = reduceMoney;
+}
+module.exports = ReductionHouse;
