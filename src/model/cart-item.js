42f6249676593d0342bf368e8de9aeb1d44d5503
yh <15248004@qq.com>
2015-02-11 16:18:32 +0800

    add getBrand() & getName()

--- src/model/cart-item.js
+++ src/model/cart-item.js
@@ -3,4 +3,12 @@ function CartItem(item, count) {
     this.count = count || 0;
 }
 
+CartItem.prototype.getBrand = function() {
+    return this.item.brand;
+};
+
+CartItem.prototype.getName = function() {
+    return this.item.name;
+};
+
 module.exports = CartItem;
