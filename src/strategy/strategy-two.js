9ddd03339acc9bc943776840a24eb9abed7d96d7
yh <15248004@qq.com>
2015-02-14 11:57:16 +0800

    create buildItemDiscountInfo()

--- src/strategy/strategy-two.js
+++ src/strategy/strategy-two.js
@@ -1,4 +1,8 @@
+var _ = require('lodash');
 var Strategy = require('./strategy');
+var DiscountHouse = require('../promotion/discount-house');
+var ItemDiscount = require('../promotion/item-discount');
+var BrandDiscount = require('../promotion/brand-discount');
 
 function StrategyTwo() {
     this.savingTotal = 0;
@@ -6,10 +10,44 @@ function StrategyTwo() {
 StrategyTwo.prototype = Object.create(Strategy.prototype);
 StrategyTwo.prototype.constructor = StrategyTwo;
 
-StrategyOne.prototype.getPromotionInfo = function(cartItems) {
+StrategyTwo.prototype.getPromotionInfo = function(cartItems) {
     var promotionInfo = '';
+    promotionInfo += this.getItemDiscountInfo(cartItems);
 
     return promotionInfo;
+};
+
+StrategyTwo.items =function() {
+    return [new DiscountHouse('可口可乐350ml', 0.95)];
+};
+
+StrategyTwo.brands =function() {
+    return [new DiscountHouse('可口可乐', 0.9)];
+};
+
+StrategyTwo.prototype.getItemDiscountInfo = function(cartItems) {
+    var _this = this;
+    var discountInfo = '';
+    var discountItems = this.findDiscountItems(cartItems, StrategyTwo.items());
+    _.forEach(discountItems, function(discountItem) {
+        discountInfo += _this.buildItemDiscountInfo(cartItems, discountItem);
+    });
+
+    return discountInfo;
+};
+
+StrategyTwo.prototype.buildItemDiscountInfo = function(cartItems, discountItem) {
+    var result = '';
+    var cartItem = _.find(cartItems, function(cartItem) {
+        return cartItem.getName() === discountItem.name;
+    });
+
+    var itemDiscount = new ItemDiscount(discountItem.rate, cartItem.getSubtotal(), discountItem.name);
+    cartItem.isPromotion = true;
+    this.savingTotal += itemDiscount.getPromotionMoney();
+    result += this.buildInfo(itemDiscount.buildPromotionName(), itemDiscount.getPromotionMoney());
+
+    return result;
 };
 
 module.exports = StrategyTwo;
