0559c6f1a06006264e6fbde0df58617e95313dff
yh <15248004@qq.com>
2015-02-14 12:13:55 +0800

    add getPromotionInfo() test

--- __tests__/strategy/strategy-two-test.js
+++ __tests__/strategy/strategy-two-test.js
@@ -8,6 +8,18 @@ describe('StrategyTwo', function() {
     var DiscountHouse = require('../../src/promotion/discount-house');
     var strategyTwo = new StrategyTwo;
 
+    describe('.getPromotionInfo()', function() {
+        var cartItems = [new CartItem(new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐'), 100),
+            new CartItem(new Item('ITEM000010', '可口可乐550ml', '瓶', 4.00, '可口可乐'), 12),
+            new CartItem(new Item('ITEM000005', '康师傅方便面', '袋', 4.50, '康师傅'), 20),
+            new CartItem(new Item('ITEM000006', '羽毛球', '个', 1.00, ''), 20)];
+        it('should return correct string', function() {
+            var result = strategyTwo.getPromotionInfo(cartItems);
+            expect(result).toBe('名称：可口可乐350ml单品打折，金额：15.00元\n');
+        });
+
+    });
+
     describe('.buildItemDiscountInfo()', function() {
         var cartItems = [new CartItem(new Item('ITEM000000', '可口可乐350ml', '瓶', 3.00, '可口可乐'), 20),
             new CartItem(new Item('ITEM000010', '可口可乐550ml', '瓶', 4.00, '可口可乐'), 12)];
