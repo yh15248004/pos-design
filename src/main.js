00b08c011c9014fbc0066366d82536c6e3a23c31
yh <15248004@qq.com>
2015-02-14 15:57:24 +0800

    use strategyTwo to pass test

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ var Inventory = require('./model/inventory');
 
     var countItems = [
         { 'ITEM000000' : 20 },
-        { 'ITEM000010' : 30 },
-        { 'ITEM000001' : 30 },
-        { 'ITEM000007' : 40 },
+        { 'ITEM000010' : 20 },
+        { 'ITEM000005' : 30 },
+        { 'ITEM000008' : 25 },
         { 'ITEM000003' : 8  },
         { 'ITEM000002' : 14 }
     ];
-    var strategyType = 1;
+    var strategyType = 2;
     var strategy = StrategyRunner.getStrategy(strategyType);
     var cart = new Cart();
     ItemFactory.createCartItems(cart, countItems);
