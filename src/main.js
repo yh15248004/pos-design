7242b59715cfc5787c2ca4c3443dbb521ac2f3f6
yh <15248004@qq.com>
2015-02-14 10:53:14 +0800

    compile strategyOne

--- src/main.js
+++ src/main.js
@@ -1,3 +1,22 @@
-function printInventory(tags) {
+var StrategyRunner = require('./strategy/strategy-runner');
+var Cart = require('./model/cart');
+var ItemFactory = require('./model/item-factory');
+var Inventory = require('./model/inventory');
 
-}
+(function printInventory() {
+
+    var countItems = [
+        { 'ITEM000000' : 20 },
+        { 'ITEM000010' : 30 },
+        { 'ITEM000001' : 30 },
+        { 'ITEM000007' : 40 },
+        { 'ITEM000003' : 8  },
+        { 'ITEM000002' : 14 }
+    ];
+    var strategyType = 1;
+    var strategy = StrategyRunner.getStrategy(strategyType);
+    var cart = new Cart();
+    ItemFactory.createCartItems(cart, countItems);
+    var inventory = new Inventory(cart);
+    console.log(inventory.toString(strategy));
+})();
