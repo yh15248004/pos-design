fd509b2bf8ec33bab843455b535a9c19e5bed642
yh <15248004@qq.com>
2015-02-16 14:14:32 +0800

    refactor printInventory() to pass StrategyThree

--- src/main.js
+++ src/main.js
@@ -7,13 +7,13 @@ var Inventory = require('./model/inventory');
 
     var countItems = [
         { 'ITEM000000' : 20 },
-        { 'ITEM000010' : 20 },
+        { 'ITEM000010' : 30 },
         { 'ITEM000005' : 30 },
         { 'ITEM000008' : 25 },
         { 'ITEM000003' : 8  },
         { 'ITEM000002' : 14 }
     ];
-    var strategyType = 2;
+    var strategyType = 3;
     var strategy = StrategyRunner.getStrategy(strategyType);
     var cart = new Cart();
     ItemFactory.createCartItems(cart, countItems);
