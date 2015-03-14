940a0dcacfc2ccfb499629378a0750901cd8e980
yh <15248004@qq.com>
2015-03-14 10:45:58 +0800

    refactor NoStrategy by add getPromotionInfo()

--- src/strategy/no-strategy.js
+++ src/strategy/no-strategy.js
@@ -1,3 +1,14 @@
+var Strategy = require('./strategy');
+
 function NoStrategy() {
+    this.savingTotal = 0;
 }
+
+NoStrategy.prototype = Object.create(Strategy.prototype);
+NoStrategy.prototype.constructor = NoStrategy;
+
+NoStrategy.prototype.getPromotionInfo = function(cartItems) {
+    return '无';
+};
+
 module.exports = NoStrategy;
