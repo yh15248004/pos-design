99f9dff0b80277fe1665380d8970d47175e604c0
yh <15248004@qq.com>
2015-03-16 13:22:01 +0800

    create Transfer & add transToHanzi()

---
+++ src/model/util/transfer.js
@@ -0,0 +1,25 @@
+function Transfer() {
+}
+
+Transfer.prototype.transToHanzi = function(value) {
+
+    var dictionary = {
+        1 : '一',
+        2 : '二',
+        3 : '三',
+        4 : '四',
+        5 : '五',
+        6 : '六',
+        7 : '七',
+        8 : '八',
+        9 : '九'
+    };
+    var result = dictionary[value / 10];
+    if(value % 10 !== 0) {
+        result.join(dictionary[value % 10]);
+    }
+
+    return result;
+};
+
+module.exports = Transfer;
